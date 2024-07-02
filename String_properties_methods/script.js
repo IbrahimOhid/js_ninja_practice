1f3011bde657512905c96b5b9773f5c38a7db5bf
unknown <ibrahimohid77@gmail.com>
2024-07-02 19:48:11 +0600

    string methods added

---
+++ String_properties_methods/script.js
@@ -0,0 +1,18 @@
+const para = '   I Love Bangladesh   ';
+
+console.log(para.toLocaleLowerCase());
+console.log(para.toUpperCase());
+
+const text = '   I Love Bangladesh   ';
+console.log(text.length);
+console.log(text.trimStart().length);
+console.log(text.trimEnd().length);
+
+const demoText = 'i am a programmer';
+console.log(demoText.slice(7, 17));
+
+const text2 = 'i live in';
+console.log(text2.concat(' eidgaon'));
+
+const paragraph = 'i live in dhaka'
+console.log(paragraph.split(' '));
