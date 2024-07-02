dc243f34d3ca3784f216d4c95faba75277ff93e4
unknown <ibrahimohid77@gmail.com>
2024-07-02 20:11:15 +0600

    array method added

---
+++ array_properties_methods/script.js
@@ -0,0 +1,17 @@
+const text = ['i', 'love', 'bangladesh'];
+console.log(text.length);
+console.log(text[2]);
+
+const user = ['rahim', 'karim', 'rahman', 'hosne'];
+console.log(user.push('abul'));
+console.log(user);
+
+console.log(user.pop());
+console.log(user);
+
+const fruits = ['banana', 'apple', 'orange', 'potato'];
+console.log(fruits.unshift('grape'));
+console.log(fruits);
+
+console.log(fruits.shift());
+console.log(fruits);
